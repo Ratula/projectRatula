dabb8662944810a9bcbf1c931b2a0673aca04cea
Ratula <ratula.chakrabarti@gmail.com>
2016-11-09 13:13:18 -0800

    Assignment 4 complete.

---
+++ Homework4/Assignment4/Assignment4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            Console.Write("Please enter a number: ");
+            string str;
+            str = Console.ReadLine();
+            int n = int.Parse(str);
+            for (int m = 1; m <= n; m++)
+            {
+                int iResult = m;
+                for (int i = 1; i < m; i++)
+                {
+                    iResult = iResult * (m - i);
+                }
+                Console.WriteLine("Factorial of the number " + m + " is: " + iResult);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
